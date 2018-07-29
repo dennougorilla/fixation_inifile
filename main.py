c8bc789a11ad7e8164bc8747da59f4d1240603eb
dennougorilla <dennougorilla@gmail.com>
2018-07-30 02:53:07 +0900

    [add] main.exe

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import os
+from time import sleep
 
-path = './GameUserSettings.ini'
+path = os.getenv('LOCALAPPDATA')+'/FortniteGame/Saved/Config/WindowsClient/GameUserSettings.ini'
 
 with open(path) as f:
     l = f.readlines()
     if 'bDisableMouseAcceleration=False\n' in l:
+        print('setting...')
         index = l.index('bDisableMouseAcceleration=False\n')
+        print(str(index)+': '+l[index]),
         del l[index]
         l.insert(index, 'bDisableMouseAcceleration=True\n')
+        print(str(index)+': '+l[index]),
+    else:
+        print(l[24]),
 
 with open(path, mode='w') as f:
-    f.writelines(l)
+        print('writing...')
+        f.writelines(l)
+        print('Done')
+        sleep(10)
